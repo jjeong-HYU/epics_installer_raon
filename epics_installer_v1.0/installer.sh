3ef4805edb68f5605da7956938b34d0736ffbc4d
jjeong-HYU <99715505+jjeong-HYU@users.noreply.github.com>
2022-04-21 17:16:38 +0900

    Update installer.sh
    debugged

--- epics_installer_v1.0/installer.sh
+++ epics_installer_v1.0/installer.sh
@@ -4,17 +4,17 @@ apt -y update
 apt -y upgrade
 
 apt install make
-apt install git
+apt install git gcc g++ build-essential
 
 # .. env variable setting ..
 
-INSTALLER_PATH=${PWD##*/}
+INSTALLER_PATH=${PWD}
 
 # ..epics install..
 
-mkdir ${HOME}/epics
+mkdir /epics
 
-cd ${HOME}/epics
+cd /epics
 
 wget https://epics.anl.gov/download/base/base-7.0.6.1.tar.gz
 
@@ -24,7 +24,7 @@ cd base-7.0.6.1
 
 make
 
-echo 'export EPICS_BASE=${HOME}/epics/base-7.0.6.1' >> ~/.bashrc 
+echo 'export EPICS_BASE=/epics/base-7.0.6.1' >> ~/.bashrc 
 echo 'export EPICS_HOST_ARCH=$(${EPICS_BASE}/startup/EpicsHostArch)' >> ~/.bashrc 
 echo 'export PATH=${EPICS_BASE}/bin/${EPICS_HOST_ARCH}:${PATH}' >> ~/.bashrc 
 
@@ -32,35 +32,35 @@ source ~/.bashrc
 
 # .. support ..
 
-mkdir ${HOME}/epics/support
+mkdir /epics/support
 
 # .. calc install ..
 
-cd ${HOME}/epics/support
+cd /epics/support
 
 git clone https://github.com/epics-modules/calc
 
 cd calc
 
-cp ${INSTALLER_PATH}/option/calc/RELEASE .
+cp ${INSTALLER_PATH}/option/calc/RELEASE ./configure/
 
 make
 
 # .. asyn install ..
 
-cd ${HOME}/epics/support
+cd /epics/support
 
 git clone https://github.com/epics-modules/asyn
 
 cd asyn
 
-cp ${INSTALLER_PATH}/option/asyn/RELEASE .
+cp ${INSTALLER_PATH}/option/asyn/RELEASE ./configure/
 
 make
 
 # .. streamdevice install ..
 
-cd ${HOME}/epics/support
+cd /epics/support
 
 git clone https://github.com/paulscherrerinstitute/StreamDevice.git
 
@@ -68,13 +68,13 @@ cd StreamDevice/
 
 rm GNUmakefile
 
-cp ${INSTALLER_PATH}/option/StreamDevice/RELEASE .
+cp ${INSTALLER_PATH}/option/StreamDevice/RELEASE ./configure/
 
 make
 
 # .. IOC install ..
 
-cd ${HOME}/epics/
+cd /epics/
 
 cp ${INSTALLER_PATH}/option/serialTest_RAON_20220420 . -r
 
