18229b5e9fc591c89c5e8a06db47cb96ad28b84a
jjeong-HYU <99715505+jjeong-HYU@users.noreply.github.com>
2022-05-16 17:25:33 +0900

    Update installer.sh

--- epics_installer_v1.0/installer.sh
+++ epics_installer_v1.0/installer.sh
@@ -1,21 +1,23 @@
 #! /bin/bash
 set -e
 
+Install_pre(){
+
 apt -y update
 apt -y upgrade
 
 apt -y install make
 apt -y install git gcc g++ build-essential
 
-# .. env variable setting ..
+}
 
-INSTALLER_PATH=${PWD}
+Install_base(){
 
 # ..epics install..
 
-mkdir /epics
+mkdir ${EPICS_PATH}
 
-cd /epics
+cd ${EPICS_PATH}
 
 wget https://epics.anl.gov/download/base/base-7.0.6.1.tar.gz
 
@@ -25,21 +27,30 @@ cd base-7.0.6.1
 
 make
 
-touch setEpicsEnv.sh
-cat /dev/null > setEpicsEnv.sh
-echo 'export EPICS_BASE=/epics/base-7.0.6.1' >> setEpicsEnv.sh
-echo 'export EPICS_HOST_ARCH=$(${EPICS_BASE}/startup/EpicsHostArch)' >> setEpicsEnv.sh
-echo 'export PATH=${EPICS_BASE}/bin/${EPICS_HOST_ARCH}:${PATH}' >> setEpicsEnv.sh
-
-echo 'source ${EPICS_BASE}/setEpicsEnv.sh' >> ~/.bashrc
+echo "Create setEpicsEnv.sh file? (1:yes / 2:no): "
+read option_env
+if [option_env == 1];then
+    touch setEpicsEnv.sh
+    cat /dev/null > setEpicsEnv.sh
+    echo 'export EPICS_BASE='${EPICS_PATH}'/base-7.0.6.1' >> setEpicsEnv.sh
+    echo 'export EPICS_HOST_ARCH=$(${EPICS_BASE}/startup/EpicsHostArch)' >> setEpicsEnv.sh
+    echo 'export PATH=${EPICS_BASE}/bin/${EPICS_HOST_ARCH}:${PATH}' >> setEpicsEnv.sh
+    echo "Adding setEpicsEnv.sh to ./bashrc? (1:yes / 2:no): "
+    read option_bashrc
+    if [option_env == 1]; then
+        echo 'source ${EPICS_BASE}/setEpicsEnv.sh' >> ~/.bashrc
+    fi
+fi
 
 # .. support ..
 
-mkdir /epics/support
+mkdir ${EPICS_PATH}/support
+}
 
+Install_calc(){
 # .. calc install ..
 
-cd /epics/support
+cd ${EPICS_PATH}/support
 
 git clone https://github.com/epics-modules/calc
 
@@ -49,9 +60,12 @@ cp ${INSTALLER_PATH}/option/calc/RELEASE ./configure/
 
 make
 
+}
+
+Install_asyn(){
 # .. asyn install ..
 
-cd /epics/support
+cd ${EPICS_PATH}/support
 
 git clone https://github.com/epics-modules/asyn
 
@@ -61,9 +75,12 @@ cp ${INSTALLER_PATH}/option/asyn/RELEASE ./configure/
 
 make
 
+}
+
+Install_stream(){
 # .. streamdevice install ..
 
-cd /epics/support
+cd ${EPICS_PATH}/support
 
 git clone https://github.com/paulscherrerinstitute/StreamDevice.git
 
@@ -74,10 +91,13 @@ rm GNUmakefile
 cp ${INSTALLER_PATH}/option/StreamDevice/RELEASE ./configure/
 
 make
+}
+
+Install_IOC{
 
 # .. IOC install ..
 
-cd /epics/
+cd ${EPICS_PATH}
 
 cp ${INSTALLER_PATH}/option/serialTest_RAON_20220420 . -r
 
@@ -86,5 +106,40 @@ cd serialTest_RAON_20220420
 make clean
 
 make
+}
+
+
+
+
+# .. env variable setting ..
+
+INSTALLER_PATH=${PWD}
+
+echo "Type the path where the system is installed"
+read -p "Path: " EPICS_PATH
+
+while :
+do
+    echo "Install option:"
+    echo "0. Prerequisition"
+    echo "1. Whole system(EPICS_BASE, Asyn, Calc, StreamDevice"
+    echo "2. EPICS_BASE_7.0.6.1"
+    echo "3. Calc"
+    echo "4. Asyn(Should be installed after calc was installed)"
+    echo "5. StreamDevice(Should be installed after calc,asyn was installed)"
+    echo "6. RAON_Serial_IOC(Should be installed after whole system was installed)"
+    echo "9. exit"
+    read -p "Type your option: " option_install
+    case ${option_install} in
+        0) Install_pre ;;
+        1) Install_pre Install_base Install_calc Install_asyn Install_stream Install_IOC ;;
+        2) Install_base ;;
+        3) Install_calc ;;
+        4) Install_asyn ;;
+        5) Install_stream ;;
+        6) Install_IOC ;;
+        9) exit 0 ;;
+    esac
+done
 
 exit 0
