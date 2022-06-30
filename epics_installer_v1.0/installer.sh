f72dddab58ebd1f6903d7c3fe0e0333b92eb1194
jjeong-HYU <99715505+jjeong-HYU@users.noreply.github.com>
2022-06-30 17:56:27 +0900

    Update installer.sh

--- epics_installer_v1.0/installer.sh
+++ epics_installer_v1.0/installer.sh
@@ -603,6 +603,27 @@ echo 'CALC=$(SUPPORT)/calc' >> configure/RELEASE
 make clean
 
 make
+
+cd ${EPICS_PATH}
+
+cp ${INSTALLER_PATH}/option/testIOC . -r
+
+cd testIOC
+
+rm configure/RELEASE
+touch configure/RELEASE
+# echo 'TEMPLATE_TOP=$(EPICS_BASE)/templates/makeBaseApp/top' >> configure/RELEASE
+echo 'EPICS_BASE='${EPICS_PATH}'/base-7.0.6.1' >> configure/RELEASE
+echo 'SUPPORT=$(EPICS_BASE)/../support' >> configure/RELEASE
+echo 'ASYN=$(SUPPORT)/asyn' >> configure/RELEASE
+echo 'STREAM=$(SUPPORT)/StreamDevice' >> configure/RELEASE
+echo 'CALC=$(SUPPORT)/calc' >> configure/RELEASE
+echo 'SNCSEQ=$(SUPPORT)/seq' >> configure/RELEASE
+
+make clean
+
+make
+
 }
 
 Install_whole_exceptArchappl(){
